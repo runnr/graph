93363f63b54148db32b225c66aed9b0684718a39
Clemens Damke <admin@cortys.de>
2016-08-24 00:28:23 +0200

    GraphContainer now supports UpdateEmitter and Persistable
    setting a graph will trigger updates for UpdateEmitter classes, and
    graph changes will trigger persist for Persistable classes.

--- GraphContainer.js
+++ GraphContainer.js
@@ -2,9 +2,13 @@
 
 const { Mixin } = require("mixwith");
 
+const UpdateEmitter = require("../helpers/UpdateEmitter");
+
 const Graph = require("./Graph");
 
 const graph = Symbol("graph");
+
+const eventType = UpdateEmitter.type.change("graph");
 
 module.exports = Mixin(superclass => class GraphContainer extends superclass {
 	get graph() {
@@ -13,8 +17,22 @@ module.exports = Mixin(superclass => class GraphContainer extends superclass {
 
 	set graph(val) {
 		if(!(val instanceof Graph))
-			throw new TypeError("Runner#graph has to be an instance of Graph.");
+			throw new TypeError(`${this.constructor.name}#graph has to be an instance of Graph.`);
+
+		if(val === this[graph])
+			return;
+
+		if(this.persist) {
+			if(this[graph])
+				this[graph].removeListener("update", this.persist);
+
+			if(val)
+				val.on("update", this.persist);
+		}
 
 		this[graph] = val;
+
+		if(this[UpdateEmitter.update])
+			this[UpdateEmitter.update](eventType, val);
 	}
 });
